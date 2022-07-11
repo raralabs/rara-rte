7431475926cdd5d4d64a10623ac33fbfeb19ab69
prasid444 <prasidkarki95@gmail.com>
2022-07-11 14:18:15 +0545

    fix:tsdx config

--- tsdx.config.js
+++ tsdx.config.js
@@ -4,6 +4,16 @@ module.exports = {
         config.plugins.push(
             postcss({
                 modules: true,
+                plugins: [
+                    autoprefixer(),
+                    cssnano({
+                        preset: 'default',
+                    }),
+                ],
+                // Append to <head /> as code running
+                inject: true,
+                // Keep it as false since we don't extract to css file anymore
+                extract: false,
             })
         );
         return config;
