cf040954fc11999341fba691e2eeb681d3d4cb07
prasid444 <prasidkarki95@gmail.com>
2022-07-12 12:21:07 +0545

    fix:export css to single  file  config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,18 +1,21 @@
 // import postcss from 'rollup-plugin-postcss'
 const postcss = require('rollup-plugin-postcss');
 const images = require('@rollup/plugin-image');
+const path=require('path');
 
 module.exports = {
     rollup(config, options) {
         config.plugins.push(
             postcss({
-                modules: true,
+                // modules: true,
                 plugins: [
                 ],
+                extract:path.resolve('dist/rara-rte.css'),
+
                 // Append to <head /> as code running
-                inject: false,
+                // inject: false,
                 // Keep it as false since we don't extract to css file anymore
-                extract: true,
+                // extract: true,
             })
         );
         return config;
