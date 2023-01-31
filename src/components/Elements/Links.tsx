8574b7d3d11096d399e208ba716ac752ab05ed95
pankajkoirala <pankaj.koirala.77@gmail.com>
2023-01-31 14:40:46 +0545

    fix: merge

--- src/components/Elements/Links.tsx
+++ src/components/Elements/Links.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
-import { BlockQuoteElement, LinkElement } from '../../types';
+import {
+  BlockQuoteElement,
+  LinkElement as LinkElementprops,
+} from '../../types';
 interface LinkElementProps {
-  attributes?: Record<string,string|boolean>;
+  attributes?: Record<string, string | boolean>;
   children?: React.ReactNode;
-  element?: BlockQuoteElement | LinkElement;
+  element?: BlockQuoteElement | LinkElementprops;
 }
 const LinkElement = ({ attributes, children, element }: LinkElementProps) => {
   if (element && 'url' in element) {
     return (
-      <a href={element.url} {...attributes}>
+      <a href={element.url} {...attributes} target="_blank">
         {children}
       </a>
     );
